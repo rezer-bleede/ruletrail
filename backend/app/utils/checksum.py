734bab722f12f6325cb1a2a89a03fda5f91309a3
rezer bleede <remisharoon.aws@gmail.com>
2025-10-27 22:05:01 +0400

    Revert "feat: deliver ruletrail mvp"

--- backend/app/utils/checksum.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-from typing import Iterable
-
-
-def compute_checksum(values: Iterable[str]) -> str:
-    sha = hashlib.sha256()
-    for value in values:
-        sha.update(value.encode("utf-8"))
-    return sha.hexdigest()
